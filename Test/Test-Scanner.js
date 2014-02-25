af9af1ac14098497f99c82122c27e39210e1251b
Abdul Alzaid <aalzaid1@lion.lmu.edu>
2014-02-24 17:25:01 -0800

    editing some files

--- Test/Test-Scanner.js
+++ Test/Test-Scanner.js
@@ -1,6 +1,6 @@
-var should = require('should');
+//var should = require('should');
 var scan = require('../scanner')
-var error = require('../error')
+//var error = require('../error')
 var i = require('util').inspect
 
 describe('The scanner', function () {
@@ -83,4 +83,4 @@ describe('The scanner', function () {
       error.count.should.equal(1)
       done()
     })*/
-  })})
+  })
